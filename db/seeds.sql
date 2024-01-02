b3d4f16d936b91ec2afc058fc6f32f40de396ff8
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-01-01 19:17:03 -0500

    department options added, role titles, salary, and department ids added

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES ('Engineering'),
+        ('Finance'),
+        ('Interconnected'),
+        ('Sales'),
+        ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Engineering Manager', 120000, 1),
+('Engineering Lead', 100000, 1),
+('Staff Engineer', 80000, 1),
+('Finance Manager', 85000, 2),
+('Accountant', 70000, 2),
+('Brand Advocate Manager', 95000, 3),
+('Brand Advocate Sr.Analyst', 83000, 3),
+('Brand Advocate Analyst', 70000, 3),
+('Sales Manager', 80000, 4),
+('Salesperson', 70000, 4),
+('Legal Manager', 200000, 5),
+('Lawyer', 100000, 5);
